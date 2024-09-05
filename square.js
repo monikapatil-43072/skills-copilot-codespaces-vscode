d40b14f9ea3a0a2f6eb4affe3511a3a2dabeef51
monikapatil-43072 <monika.patil@thoughtworks.com>
2024-09-05 02:53:01 +0000

    refactore @workspace text

--- square.js
+++ square.js
@@ -1,6 +1,7 @@
-// @workspace duplicate squareNumber function
-// function squareNumber(num) {
-//     return num * num;
-// }
+// @workspace where is the squareNumber function is being used?
+// A. square.js
+
+// @workspace where is the squareNumber function is defined?
+// A. square.js
 
 
